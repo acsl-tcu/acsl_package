817f2f64e1a29634c5c8d901c3a7197def914400
kasekiguchi <kasekiguchi@gmail.com>
2024-12-06 14:57:44 +0900

    Update setup.py

--- 2_ros_packages/yolo/setup.py
+++ 2_ros_packages/yolo/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 package_name = "yolo"
 
 setup(
     name=package_name,
     version="0.0.0",
-    packages=[],
+    packages=find_packages(exclude=["test"]),
     py_modules=[
         "src.talker",
         "src.listener",
