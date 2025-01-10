4f3cca1cfdb207017d7d15406deb62aae2043175
kasekiguchi <kasekiguchi@gmail.com>
2025-01-11 05:25:33 +0900

    toolbox

--- 1_launcher/launch_slam.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/bin/bash
-$(echo "exec ros2 launch slam_toolbox online_async_launch.py --remap __ns:=/$HOSTNAME")
---
+++ 1_launcher/launch_slam_toolbox.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/bash
+$(echo "exec ros2 launch slam_toolbox online_async_launch.py")
