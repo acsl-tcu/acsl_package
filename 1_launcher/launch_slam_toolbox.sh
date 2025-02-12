13e7dcb5c7468e97c91bfc212404b9b8e1aca518
kasekiguchi <kasekiguchi@gmail.com>
2025-02-12 21:07:26 +0900

    rviz

--- 1_launcher/launch_slam_toolbox.sh
+++ 1_launcher/launch_slam_toolbox.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/bash
 
 #if [[ $1 == "save_map" ]]; then
+source /opt/ros/${ROS_DISTRO}/setup.bash
 case $1 in
 "save_map")
   echo "Save map"
@@ -11,12 +12,12 @@ case $1 in
   # ros2 run ros_gz_bridge parameter_bridge /scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan
   # ros2 run ros_gz_bridge parameter_bridge /model/Robot1/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V --ros-args -r /model/Robot1/tf:=/tf
   # ros2 run ros_gz_bridge parameter_bridge /model/Robot1/odometry@nav_msgs/msg/Odometry@gz.msgs.Odometry --ros-args -r /model/Robot1/odometry:=/odom
-  ros2 launch slam_toolbox online_sync_launch.py slam_params_file:=/common/ros_launcher/launch_slam_toolbox/rf_robot_slam.yaml
+  ros2 launch slam_toolbox online_async_launch.py slam_params_file:=/common/ros_launcher/launch_slam_toolbox/rf_robot_slam.yaml
   ;;
 "matching")
   ros2 launch slam_toolbox localization_launch.py slam_params_file:=/common/ros_launcher/launch_slam_toolbox/rf_robot_localization.yaml
   ;;
 "rviz2")
-  rviz2
+  rviz2 -d /common/ros_launcher/launch_slam_toolbox/slam_toolbox.rviz
   ;;
 esac
