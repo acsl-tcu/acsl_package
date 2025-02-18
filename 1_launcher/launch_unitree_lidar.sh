945e8b279e0ddb568f6660651c952d8a7bd7de23
kasekiguchi <kasekiguchi@gmail.com>
2025-02-18 12:09:05 +0900

    lidar

--- 1_launcher/launch_unitree_lidar.sh
+++ 1_launcher/launch_unitree_lidar.sh
@@ -7,6 +7,6 @@ case $1 in
   rviz2 -d src/ros_packages/unilidar_sdk/unitree_lidar_ros2/src/unitree_lidar_ros2/rviz/view.rviz
   ;;
 *)
-  ros2 launch unitree_lidar_ros2 launch.py
+  ros2 launch unitree_lidar_ros2 launch.py port:=ttyUSB-UNITREE-L1
   ;;
 esac
